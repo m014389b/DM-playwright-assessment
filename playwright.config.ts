569adab9d95106a62f98254346241e07aa17db49
unknown <dmillard91@hotmail.co.uk>
2026-03-04 15:45:46 +0000

    add uncommited files

---
+++ playwright.config.ts
@@ -0,0 +1,71 @@
+import { defineConfig, devices } from '@playwright/test';
+
+module.exports = defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 1,
+
+  timeout: 5 * 60 * 1000,
+  expect: {
+    timeout: 60 * 1000,
+  },
+  reportSlowTests: null,
+
+  workers: process.env.FUNCTIONAL_TESTS_WORKERS ? 5 : 5,
+  reporter: process.env.CI ? "html" : "html",
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+        channel: "chrome",
+        trace: "retain-on-first-failure",
+        javaScriptEnabled: true,
+      },
+    },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+        screenshot: "off",
+        trace: "retain-on-first-failure",
+        javaScriptEnabled: true,
+      },
+    },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+        screenshot: "off",
+        trace: "retain-on-first-failure",
+        javaScriptEnabled: true,
+      },
+    },
+    {
+      name: "MobileChrome",
+      use: {
+        ...devices["Pixel 5"],
+        screenshot: "only-on-failure",
+        trace: "off",
+      },
+    },
+    {
+      name: "MobileSafari",
+      use: {
+        ...devices["iPhone 12"],
+        screenshot: "only-on-failure",
+        trace: "off",
+      },
+    },
+    {
+      name: "MicrosoftEdge",
+      use: {
+        ...devices["Desktop Edge"],
+        channel: "msedge",
+        screenshot: "only-on-failure",
+        trace: "off",
+      },
+    },
+  ],
+});
